b9a66f82152b6cca4ff0920b262b86aa6b8aa125
byrantwithyou <byrantwithyou@163.com>
2019-03-09 16:46:01 -0500

    fix bug for input name

--- App.js
+++ App.js
@@ -64,7 +64,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <View style={{width: 100, height: 100}}>
           <TextInput style={{height: 40, borderColor: "gray", borderWidth: 1}}
-                      onChangeText={(text) => this.setState({ text })}
+                      onChangeText={(studentName) => this.setState({ studentName })}
                       value={this.state.studentName}
           />
         </View>
